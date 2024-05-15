00b75b4d11313397e901a4ef96d35203bbb51929
Andrew <andytgarcia27@gmail.com>
2024-05-15 11:36:23 -0700

    chore: Add Square class for representing a square on the chessboard

---
+++ header/Square.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Piece.hpp"
+
+class Square {
+    private:
+        Piece* piece;
+        std::pair<int, int> position;
+    public:
+        Square();
+        Square(Piece* piece, std::pair<int, int> position);
+        Piece* getPiece();
+        std::pair<int, int> getIndex();
+        void setPiece(Piece* piece);
+        void setIndex(std::pair<int, int> index);
+};
