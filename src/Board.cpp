20fb0e604f06483ff68b085bc38f44ecb8490928
Andrew <112200249+andytgarcia@users.noreply.github.com>
2024-05-22 11:44:42 -0700

    Initialized Pieces on Board (#24)

--- src/Board.cpp
+++ src/Board.cpp
@@ -1,9 +1,28 @@
 #include "../header/Board.hpp"
+#include "../header/Pieces/Rook.hpp"
+#include "../header/Pieces/Knight.hpp"
+#include "../header/Pieces/Bishop.hpp"
+#include "../header/Pieces/King.hpp"
+#include "../header/Pieces/Queen.hpp"
+#include "../header/Pieces/Pawn.hpp"
 
 using std::endl;
 
 Board::Board() {
+  board[0][0] = new Square(new Rook(PieceColor::BLACK), std::make_pair(0, 0));
+  board[0][1] = new Square(new Knight(PieceColor::BLACK), std::make_pair(0, 1));
+  board[0][2] = new Square(new Bishop(PieceColor::BLACK), std::make_pair(0, 2));
+  board[0][3] = new Square(new King(PieceColor::BLACK), std::make_pair(0, 3));
+  board[0][4] = new Square(new Queen(PieceColor::BLACK), std::make_pair(0, 4));
+  board[0][5] = new Square(new Bishop(PieceColor::BLACK), std::make_pair(0, 5));
+  board[0][6] = new Square(new Knight(PieceColor::BLACK), std::make_pair(0, 6));
+  board[0][7] = new Square(new Rook(PieceColor::BLACK), std::make_pair(0, 7));
+
   for (int i = 0; i < 8; i++) {
+    board[1][i] = new Square(new Pawn(PieceColor::BLACK), std::make_pair(1, i));
+  }
+
+  for (int i = 2; i < 6; i++) {
     for (int j = 0; j < 8; j++) {
       board[i][j] = new Square(nullptr, std::make_pair(i, j));
     }
@@ -12,26 +31,37 @@ Board::Board() {
 
 Board::~Board() {
   for (int i = 0; i < 8; i++) {
-    for (int j = 0; j < 8; j++) {
-      delete board[i][j];
-    }
+    board[1][i] = new Square(new Pawn(PieceColor::BLACK), std::make_pair(1, i));
   }
-}
 
-Board::Board(Square* newBoard[8][8]) {
-  for (int i = 0; i < 8; i++) {
+  for (int i = 2; i < 6; i++) {
     for (int j = 0; j < 8; j++) {
-      board[i][j] = newBoard[i][j];
+      board[i][j] = new Square(nullptr, std::make_pair(i, j));
     }
   }
+
+  for (int i = 0; i < 8; i++) {
+    board[6][i] = new Square(new Pawn(PieceColor::WHITE), std::make_pair(6, i));
+  }
+
+  board[7][0] = new Square(new Rook(PieceColor::WHITE), std::make_pair(7, 0));
+  board[7][1] = new Square(new Knight(PieceColor::WHITE), std::make_pair(7, 1));
+  board[7][2] = new Square(new Bishop(PieceColor::WHITE), std::make_pair(7, 2));
+  board[7][3] = new Square(new King(PieceColor::WHITE), std::make_pair(7, 3));
+  board[7][4] = new Square(new Queen(PieceColor::WHITE), std::make_pair(7, 4));
+  board[7][5] = new Square(new Bishop(PieceColor::WHITE), std::make_pair(7, 5));
+  board[7][6] = new Square(new Knight(PieceColor::WHITE), std::make_pair(7, 6));
+  board[7][7] = new Square(new Rook(PieceColor::WHITE), std::make_pair(7, 7));
+
+
 }
 
 string Board::displayBoard(bool flipped) {
   std::stringstream ss;
 
-  // loop through the board and display the pieces in each square and go to the next
+  // loop through the board and display the pieces in each new Square and go to the next
   // start each row with the number that is corresponds to
-  string lineWidth = "   =================================";
+  string lineWidth = "   ==========================================";
 
   int startingIndex = 0;
   int endingIndex = 8;
@@ -50,7 +80,7 @@ string Board::displayBoard(bool flipped) {
     for (int j = startingIndex; flipped ? j > endingIndex : j < endingIndex; j += increment) {
       // if piece does not exist, print a dash, otherwise print the piece
       if (board[i][j]->getPiece() == nullptr) {
-        ss << " - ";
+        ss << " -- ";
       } else {
         Piece* currentPiece = board[i][j]->getPiece();
         ss << " " << currentPiece->getSymbol() << " ";
@@ -63,9 +93,9 @@ string Board::displayBoard(bool flipped) {
 
   // print the column letters
   if (flipped) {
-    ss << "    H   G   F   E   D   C   B   A" << endl;
+    ss << "     H    G    F    E    D    C    B    A" << endl;
   } else {
-    ss << "    A   B   C   D   E   F   G   H" << endl;
+    ss << "     A    B    C    D    E    F    G    H    " << endl;
   }
   return ss.str();
 }
