d195c69eea6544eadf0366973b552434b1ec0f7d
Justin Albert <jnalbert879@gmail.com>
2024-05-23 14:02:16 -0700

    fixes

--- header/Player.hpp
+++ header/Player.hpp
@@ -7,14 +7,14 @@
 using namespace std;
 
 class Player {
-private:
-    string name;
-    PieceColor color;
+ private:
+  string name;
+  PieceColor color;
 
-public:
-    Player(PieceColor color, const string& name) : color(color), name(name) {};
-    Player() : color(PieceColor::WHITE), name("") {};
-    PieceColor getColor() {return color;}
-    string getName(){ return name; }
-    void setName(const string& name){ this->name = name; }
+ public:
+  Player(PieceColor color, const string& name) : color(color), name(name) {};
+  Player() : color(PieceColor::WHITE), name(""){};
+  PieceColor getColor() { return color; }
+  string getName() { return name; }
+  void setName(const string& name) { this->name = name; }
 };
