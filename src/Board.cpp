b23172ba432acfe1cfbd725048dea6fb9faa17fa
Justin Albert <jnalbert879@gmail.com>
2024-05-22 18:46:04 -0700

    fix build

--- src/Board.cpp
+++ src/Board.cpp
@@ -9,17 +9,17 @@
 using std::endl;
 
 Board::Board() {
-  board[0][0] = Square(new Rook(PieceColor::BLACK), std::make_pair(0, 0));
-  board[0][1] = Square(new Knight(PieceColor::BLACK), std::make_pair(0, 1));
-  board[0][2] = Square(new Bishop(PieceColor::BLACK), std::make_pair(0, 2));
-  board[0][3] = Square(new King(PieceColor::BLACK), std::make_pair(0, 3));
-  board[0][4] = Square(new Queen(PieceColor::BLACK), std::make_pair(0, 4));
-  board[0][5] = Square(new Bishop(PieceColor::BLACK), std::make_pair(0, 5));
-  board[0][6] = Square(new Knight(PieceColor::BLACK), std::make_pair(0, 6));
-  board[0][7] = Square(new Rook(PieceColor::BLACK), std::make_pair(0, 7));
+  board[0][0] = new Square(new Rook(PieceColor::BLACK), std::make_pair(0, 0));
+  board[0][1] = new Square(new Knight(PieceColor::BLACK), std::make_pair(0, 1));
+  board[0][2] = new Square(new Bishop(PieceColor::BLACK), std::make_pair(0, 2));
+  board[0][3] = new Square(new King(PieceColor::BLACK), std::make_pair(0, 3));
+  board[0][4] = new Square(new Queen(PieceColor::BLACK), std::make_pair(0, 4));
+  board[0][5] = new Square(new Bishop(PieceColor::BLACK), std::make_pair(0, 5));
+  board[0][6] = new Square(new Knight(PieceColor::BLACK), std::make_pair(0, 6));
+  board[0][7] = new Square(new Rook(PieceColor::BLACK), std::make_pair(0, 7));
 
   for (int i = 0; i < 8; i++) {
-    board[1][i] = Square(new Pawn(PieceColor::BLACK), std::make_pair(1, i));
+    board[1][i] = new Square(new Pawn(PieceColor::BLACK), std::make_pair(1, i));
   }
 
   for (int i = 2; i < 6; i++) {
@@ -29,17 +29,17 @@ Board::Board() {
   }
 
   for (int i = 0; i < 8; i++) {
-    board[6][i] = Square(new Pawn(PieceColor::WHITE), std::make_pair(6, i));
+    board[6][i] = new Square(new Pawn(PieceColor::WHITE), std::make_pair(6, i));
   }
 
-  board[7][0] = Square(new Rook(PieceColor::WHITE), std::make_pair(7, 0));
-  board[7][1] = Square(new Knight(PieceColor::WHITE), std::make_pair(7, 1));
-  board[7][2] = Square(new Bishop(PieceColor::WHITE), std::make_pair(7, 2));
-  board[7][3] = Square(new King(PieceColor::WHITE), std::make_pair(7, 3));
-  board[7][4] = Square(new Queen(PieceColor::WHITE), std::make_pair(7, 4));
-  board[7][5] = Square(new Bishop(PieceColor::WHITE), std::make_pair(7, 5));
-  board[7][6] = Square(new Knight(PieceColor::WHITE), std::make_pair(7, 6));
-  board[7][7] = Square(new Rook(PieceColor::WHITE), std::make_pair(7, 7));
+  board[7][0] = new Square(new Rook(PieceColor::WHITE), std::make_pair(7, 0));
+  board[7][1] = new Square(new Knight(PieceColor::WHITE), std::make_pair(7, 1));
+  board[7][2] = new Square(new Bishop(PieceColor::WHITE), std::make_pair(7, 2));
+  board[7][3] = new Square(new King(PieceColor::WHITE), std::make_pair(7, 3));
+  board[7][4] = new Square(new Queen(PieceColor::WHITE), std::make_pair(7, 4));
+  board[7][5] = new Square(new Bishop(PieceColor::WHITE), std::make_pair(7, 5));
+  board[7][6] = new Square(new Knight(PieceColor::WHITE), std::make_pair(7, 6));
+  board[7][7] = new Square(new Rook(PieceColor::WHITE), std::make_pair(7, 7));
 
 
 }
