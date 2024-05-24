59a7b9299e318730f9a3f4262353ee263a2f5bd3
Justin Albert <65417105+jnalbert@users.noreply.github.com>
2024-05-23 22:13:09 -0700

    Create display board game class (#27)
    * merge
    
    * add display board class
    
    * fix
    
    * fixes

--- header/Board.hpp
+++ header/Board.hpp
@@ -1,7 +1,8 @@
 #pragma once
-#include "Square.hpp"
-#include <string>
 #include <sstream>
+#include <string>
+
+#include "Square.hpp"
 #include "Player.hpp"
 
 using std::string;
@@ -9,9 +10,9 @@ using std::string;
 class Board {
   Square* board[8][8];
 
-  public: 
-    Board();
-    ~Board();
-    string displayBoard(bool flipped);
-    std::pair<bool, PieceColor> checkMate();
+ public:
+  Board();
+  ~Board();
+  Square* getSquare(int row, int col) const;
+  std::pair<bool, PieceColor> checkMate();
 };
