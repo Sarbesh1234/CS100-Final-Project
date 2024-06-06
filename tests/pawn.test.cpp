c670df82ed097e9fd14b09ada8a2b136404694db
Sarbesh1234 <sankarsarbesh@gmail.com>
2024-06-06 00:29:24 -0700

    Fixed Testing for Pawn Diaganol

--- tests/pawn.test.cpp
+++ tests/pawn.test.cpp
@@ -12,7 +12,7 @@ TEST(PawnSuite, PawnInMiddleFirstMove) {
         }
     }
     board[3][3] = new Square(new Pawn(PieceColor::BLACK), std::make_pair(3, 3));
-    board[3][5] = new Square(new Pawn(PieceColor::BLACK), std::make_pair(3, 5));
+    board[3][5] = new Square(new Pawn(PieceColor::WHITE), std::make_pair(3, 5));
     
     Pawn pawn(PieceColor::WHITE);
 
@@ -21,7 +21,7 @@ TEST(PawnSuite, PawnInMiddleFirstMove) {
 
     EXPECT_TRUE(pawn.isValidMove(std::make_pair(3, 4)));
     EXPECT_TRUE(pawn.isValidMove(std::make_pair(3, 3)));
-    EXPECT_TRUE(pawn.isValidMove(std::make_pair(3, 5)));
+    EXPECT_FALSE(pawn.isValidMove(std::make_pair(3, 5)));
     EXPECT_TRUE(pawn.isValidMove(std::make_pair(2, 4)));
 
     EXPECT_FALSE(pawn.isValidMove(std::make_pair(4, 4)));
@@ -38,7 +38,7 @@ TEST(BlackPawnSuite, PawnInMiddleFirstMove) {
         }
     }
     board[5][3] = new Square(new Pawn(PieceColor::WHITE), std::make_pair(5, 3));
-    board[5][5] = new Square(new Pawn(PieceColor::WHITE), std::make_pair(5, 5));
+    board[5][5] = new Square(new Pawn(PieceColor::BLACK), std::make_pair(5, 5));
     
     Pawn pawn(PieceColor::BLACK);
 
@@ -46,7 +46,7 @@ TEST(BlackPawnSuite, PawnInMiddleFirstMove) {
 
     EXPECT_TRUE(pawn.isValidMove(std::make_pair(5, 4)));
     EXPECT_TRUE(pawn.isValidMove(std::make_pair(5, 3)));
-    EXPECT_TRUE(pawn.isValidMove(std::make_pair(5, 5)));
+    EXPECT_FALSE(pawn.isValidMove(std::make_pair(5, 5)));
     EXPECT_TRUE(pawn.isValidMove(std::make_pair(6, 4)));
 
     EXPECT_FALSE(pawn.isValidMove(std::make_pair(4, 4)));
@@ -98,7 +98,7 @@ TEST(PawnSuite, PawnInCorner) {
         }
     }
     board[6][1] = new Square(new Pawn(PieceColor::BLACK), std::make_pair(6, 1));
-    board[6][6] = new Square(new Pawn(PieceColor::BLACK), std::make_pair(6, 6));
+    board[6][6] = new Square(new Pawn(PieceColor::WHITE), std::make_pair(6, 6));
     
     Pawn pawn(PieceColor::WHITE);
 
@@ -116,7 +116,7 @@ TEST(PawnSuite, PawnInCorner) {
     pawn.constructPossibleMoves(std::make_pair(7, 7),board);
 
     EXPECT_TRUE(pawn.isValidMove(std::make_pair(6, 7)));
-    EXPECT_TRUE(pawn.isValidMove(std::make_pair(6, 6)));
+    EXPECT_FALSE(pawn.isValidMove(std::make_pair(6, 6)));
 
     EXPECT_FALSE(pawn.isValidMove(std::make_pair(7, 7)));
     EXPECT_FALSE(pawn.isValidMove(std::make_pair(5, 7)));
@@ -132,7 +132,7 @@ TEST(BlackPawnSuite, PawnInCorner) {
         }
     }
     board[1][6] = new Square(new Pawn(PieceColor::WHITE), std::make_pair(1, 6));
-    board[1][1] = new Square(new Pawn(PieceColor::WHITE), std::make_pair(1, 1));
+    board[1][1] = new Square(new Pawn(PieceColor::BLACK), std::make_pair(1, 1));
     //top left corner first move
     Pawn pawn(PieceColor::BLACK);
 
@@ -149,7 +149,7 @@ TEST(BlackPawnSuite, PawnInCorner) {
     pawn.constructPossibleMoves(std::make_pair(0, 0),board);
 
     EXPECT_TRUE(pawn.isValidMove(std::make_pair(1, 0)));
-    EXPECT_TRUE(pawn.isValidMove(std::make_pair(1, 1)));
+    EXPECT_FALSE(pawn.isValidMove(std::make_pair(1, 1)));
 
     EXPECT_FALSE(pawn.isValidMove(std::make_pair(0, 0)));
     EXPECT_FALSE(pawn.isValidMove(std::make_pair(2, 0)));
